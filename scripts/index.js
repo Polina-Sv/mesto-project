d1a760854371b69c9867c819172518ffc61992cb
PolinaSv <polinasvyatkina0@gmail.com>
2025-02-08 15:48:27 +0300

    feat: insert-card

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,23 @@
-// @todo: Темплейт карточки
+import initialCards from "./cards.js";
+
+const cardTemplate = document.querySelector('#card-template').content;
+const card = cardTemplate.querySelector('.card');
+const cardContainer = document.querySelector('.places__list');
+
+function createCard(item) {
+  const cardElement = card.cloneNode(true);
+
+  cardElement.querySelector('.card__title').textContent = item.name;
+  cardElement.querySelector('.card__image').src = item.link;
+
+  return cardElement;
+}
+
+initialCards.forEach(item => {
+  const card = createCard(item);
+  cardContainer.append(card);
+})
+
 
 // @todo: DOM узлы
 
