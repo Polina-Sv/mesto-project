ccf2db8321edc14cc12607282d1ce9f1cb7336b1
PolinaSv <polinasvyatkina0@gmail.com>
2025-03-10 11:15:33 +0300

    feat: close modal by esc and overlay

--- scripts/index.js
+++ scripts/index.js
@@ -31,12 +31,34 @@ function createCard(item) {
   return cardElement;
 }
 
-function openModal(popup) {      
+const closePopupOverlay = (evt) => {
+    const isClosest = evt.target.closest('.popup__content');
+    const isPopupOpened = document.querySelector('.popup_is-opened');
+    
+    console.log(evt.target);
+
+    if(!isClosest && isPopupOpened) {
+        closeModal(isPopupOpened);
+    }
+}
+
+const closePopupEsc = (evt) => {
+    if (evt.key === "Escape"  || evt.keyCode === 27) {
+        closeModal(document.querySelector('.popup_is-opened'));
+    }
+}
+
+function openModal(popup) {    
     popup.classList.add('popup_is-opened');
+    popup.addEventListener('click', closePopupOverlay);
+    document.addEventListener('keydown', closePopupEsc);
+    console.log('1');
 }
 
 function closeModal(popup) {
     popup.classList.remove('popup_is-opened');
+    popup.removeEventListener('click', closePopupOverlay);
+    document.removeEventListener('keydown', closePopupEsc);
 }
 
 
