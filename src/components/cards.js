af554d1dde9376a7de7b8dd0d6882551efa22f48
PolinaSv <polinasvyatkina0@gmail.com>
2025-05-27 17:31:47 +0300

    feat: add card

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,7 +1,42 @@
-import { openModal } from "./modal.js";
+import {closeModal, openModal} from "./modal.js";
 import { getInitialCards, addNewCard, deleteCardApi } from './api.js';
 
 const cardsContainer = document.querySelector('.places__list');
+const addButton = document.querySelector('.profile__add-button');
+const addModal = document.querySelector('.popup_type_new-card');
+const addForm = addModal.querySelector('.popup__form');
+const placeNameInput = addForm.querySelector('.popup__input_type_card-name');
+const linkInput = addForm.querySelector('.popup__input_type_url');
+
+export function initCardAdding(userId) {
+  addButton.addEventListener('click', () => {
+    addForm.reset();
+    openModal(addModal);
+  });
+
+  addForm.addEventListener('submit', (evt) => handleAddCardSubmit(evt, userId));
+}
+
+function handleAddCardSubmit(evt, userId) {
+  evt.preventDefault();
+  
+  const submitButton = addForm.querySelector('.popup__button');
+  const initialText = submitButton.textContent;
+  submitButton.textContent = 'Создание...';
+
+  addNewCard(placeNameInput.value, linkInput.value)
+    .then(newCard => {
+      const cardElement = createCardElement(newCard, userId);
+      cardsContainer.prepend(cardElement); 
+      closeModal(addModal);
+    })
+    .catch(err => {
+      console.error('Ошибка добавления карточки:', err);
+    })
+    .finally(() => {
+      submitButton.textContent = initialText;
+    });
+}
 
 export function loadAndRenderCards(userId) {
   return getInitialCards()
@@ -22,7 +57,6 @@ function renderCards(cards, userId) {
     cardsContainer.append(cardElement);
   });
 }
-//const imagePopup = document.querySelector('.popup_type_image');
 
 function createCardElement(cardData, userId) {
   const cardTemplate = document.querySelector('#card-template').content;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import './pages/index.css';
 import enableValidation from "./components/validate.js";
-import {closeModal, openModal} from "./components/modal.js";
+import {closeModal} from "./components/modal.js";
 import { loadProfileData, initProfileEditing } from './components/profile.js';
-import { loadAndRenderCards } from './components/cards.js';
+import { loadAndRenderCards, initCardAdding } from './components/cards.js';
 
 function initApp() {
   loadProfileData()
     .then(userData => {
-      return loadAndRenderCards(userData._id);
-    })
-    .then(() => {
+      loadAndRenderCards(userData._id);
       initProfileEditing();
+      initCardAdding(userData._id);
     })
     .catch(err => {
       console.error('Ошибка инициализации:', err);
@@ -22,7 +21,6 @@ document.addEventListener('DOMContentLoaded', initApp);
 const profilePopup = document.querySelector('.popup_type_edit');
 const cardPopup = document.querySelector('.popup_type_new-card');
 const imagePopup = document.querySelector('.popup_type_image');
-const cardContainer = document.querySelector('.places__list');
 
 profilePopup.classList.add('popup_is-animated');
 cardPopup.classList.add('popup_is-animated');
@@ -34,26 +32,5 @@ popupCloseBtns.forEach((button) => {
         closeModal(button.closest('.popup'));
     })
 })
-
-
-const cardAddBtn = document.querySelector('.profile__add-button');
-cardAddBtn.addEventListener('click', evt => {
-    openModal(cardPopup);
-});
-
-const inputCardName = cardPopup.querySelector('.popup__input_type_card-name');
-const inputUrl = cardPopup.querySelector('.popup__input_type_url');
-const cardForm = document.forms['new-place'];
-
-cardForm.addEventListener('submit', evt => {
-    evt.preventDefault();
-    const info = {
-        name: inputCardName.value,
-        link: inputUrl.value,
-    }
-    const card = createCard(info);
-    cardContainer.prepend(card);
-    closeModal(cardPopup);
-});
 
 enableValidation();
